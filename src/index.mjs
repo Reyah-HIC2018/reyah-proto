b85b760aa915e7d7c1af77498c2c0ee0a2482fec
Arthur Knoepflin <arthur.knoepflin@epitech.eu>
2018-10-14 14:44:41 +0200

    [+] CORS fixed-2

--- src/index.mjs
+++ src/index.mjs
@@ -15,7 +15,7 @@ app.use(body_parser.json({ limit: "1000gb" }));
 
 app.use((req, res, next) => {
     res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', '*');
+    res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
     next();
 });
 app.use("/models", model_route);
