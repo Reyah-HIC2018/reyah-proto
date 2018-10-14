60a24baad133d60e01073ca5a7db0efcd98e326b
Thibaut Cornolti <thibaut@cornolti.eu>
2018-10-14 11:16:35 +0200

    [+] FIXED BASTIEN AND CEDRIC'S ERRORS

--- src/routes/generate.mjs
+++ src/routes/generate.mjs
@@ -33,12 +33,18 @@ router.get('/:id', async (req, res) => {
     try {
         const tmplt = await Templates.findById(id).exec();
         const usr = JSON.parse(JSON.stringify(await Data.find({}).exec()));
-        const data = {};
+        const data = [];
 
         tmplt.fields.forEach((elem) => {
             if (!usr[0][elem.name])
-                return res.status(400).json({message: "Missing fields" })   
-            data[elem.name] = usr[0][elem.name];
+                return res.status(400).json({message: "Missing fields", usr: usr })
+            const obj = {};
+            obj[data[elem.name]] = usr[0][elem.name];
+            obj.x1 = elem.x1;
+            obj.x2 = elem.x2;
+            obj.y1 = elem.y1;
+            obj.y2 = elem.y2;
+            data.push(obj);
         });
 
         console.log(tmplt);
