fc125e598042bb4d3b56f9696ea2fc1941b00c52
Nicolas Polomack <nipolom@live.fr>
2018-10-14 12:45:44 +0200

    Fixed issues

--- src/routes/generate.mjs
+++ src/routes/generate.mjs
@@ -9,18 +9,16 @@ const router = express.Router();
 async function imageFill(template, data, output) {
     return new Promise((resolve, reject) => {
         Jimp.read(`${template.path}`, async (err, file) => {
-            if (err) 
+            if (err)
                 return reject(err);
             const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-            for (let field of template.fields) {
-                if (data[field.name]) {
+            for (const field of template.fields)
+                if (data[field.name])
                     file.print(font, field.x1, field.y1, {
                         text: data[field.name],
                         alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
                         alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM
-                      });
-                }
-            }
+                    });
             file.write(output);
             return resolve();
         });
@@ -28,16 +26,15 @@ async function imageFill(template, data, output) {
 }
 
 router.get('/:id', async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     try {
-
         const tmplt = await Templates.findById(id).exec();
         const usr = JSON.parse(JSON.stringify(await Data.find({}).exec()));
         const data = [];
 
         tmplt.fields.forEach((elem) => {
             if (!usr[0][elem.name])
-                return res.status(400).json({message: "Missing fields", usr: usr })
+                return res.status(400).json({ message: "Missing fields", usr: usr })
             const obj = {};
             obj[data[elem.name]] = usr[0][elem.name];
             obj.x1 = elem.x1;
@@ -48,13 +45,14 @@ router.get('/:id', async (req, res) => {
         });
 
         console.log(tmplt);
-        imageFill(tmplt, data, path.join("static", tmplt.name + '_filled.jpg'));
-        res.redirect(path.join("/static", tmplt.name + '_filled.jpg'));
-    } catch(err) {
+        const path = path.join("static", `${tmplt.name}_filled.jpg`);
+        await imageFill(tmplt, data, path);
+        res.redirect(path);
+    } catch (err) {
         console.log(err);
-        res.status(404).json({message: "Unknown model", path: path.join("/static", tmplt.name + '_filled.jpg')})
+        res.status(404).json({ message: "Unknown model" });
     }
-    
+
 });
 
 export default router;
--- src/routes/models.mjs
+++ src/routes/models.mjs
@@ -12,14 +12,13 @@ const read_dir = promisify(readdir);
 
 export const router = express.Router();
 
-
 async function genThumb(file_path) {
     return new Promise((resolve, reject) => {
         Jimp.read(file_path, async (err, file) => {
             if (err)
                 return reject(err);
             const thumb = path.parse(file_path);
-            file.resize(210, 297).write(path.join(thumb.dir, thumb.name + '_thumb' + thumb.ext));     
+            file.resize(210, 297).write(path.join(thumb.dir, thumb.name + '_thumb' + thumb.ext));
             return resolve();
         });
     });
@@ -27,16 +26,15 @@ async function genThumb(file_path) {
 
 router.put("/:id", async (req, res) => {
     const { data } = req.body;
-    console.log(data, req.body);
-    if (data === undefined)
+    if (data == undefined)
         return res.status(400).json({ error: "missing data" });
     try {
         await Promise.all(
             Object.entries(data).map(([key, val]) =>
-                Data.updateOne({  }, { [key]: val }).exec()));
+                Data.updateOne({}, { [key]: val }).exec()));
         res.status(400).json(data);
-    } catch (err) {
-        res.status(400).json({ error: err });
+    } catch (error) {
+        res.status(400).json({ error });
     }
 });
 
@@ -45,7 +43,7 @@ router.post("/:name", async (req, res) => {
     const { name } = req.params;
 
     if (!template || !fields || !name)
-        return res.status(400).json({ error: "missing data" });
+        return res.status(400).json({ error: "Missing data" });
     try {
         const files = await read_dir("static");
         const path = (() => {
@@ -59,9 +57,9 @@ router.post("/:name", async (req, res) => {
         await genThumb(`static/${path}.jpg`);
         await Templates.create({ name, path: `static/${path}`, fields, format: "jpg" });
         res.status(200).json({ name, path: `static/${path}`, fields, format: "jpg" });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: err });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
     }
 });
 
@@ -70,10 +68,11 @@ router.get("/:id", async (req, res) => {
     try {
         const results = await Templates.findById(id).exec()
             .then(({ name, path, fields, format }) =>
-                ({ name, path: `${path}.${format}`, thumb: `${path}_thumb.${format}`, fields: fields.map(({ name, value }) =>
-                    ({ name, value })), format }));
-        let user = (await Data.find({  }).exec())[0];
-        user = JSON.parse(JSON.stringify(user));
+                ({
+                    name, path: `${path}.${format}`, thumb: `${path}_thumb.${format}`, fields: fields.map(({ name, value }) =>
+                        ({ name, value })), format
+                }));
+        const user = JSON.parse(JSON.stringify((await Data.find({}).exec())[0]));
         results.fields.forEach(elem => {
             if (user[elem.name])
                 elem.value = user[elem.name];
