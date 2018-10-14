123b7a0cc845cedd965c07b7037be456cfcea3ec
Nicolas Polomack <nipolom@live.fr>
2018-10-14 14:56:48 +0200

    Fixed issues

--- src/routes/models.mjs
+++ src/routes/models.mjs
@@ -33,7 +33,7 @@ router.put("/:id", async (req, res) => {
         await Promise.all(
             Object.entries(data).map(([key, val]) =>
                 Data.updateOne({}, { [key]: val }).exec()));
-        res.status(400).json(data);
+        res.status(200).json(data);
     } catch (error) {
         res.status(400).json({ error });
     }
