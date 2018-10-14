864a5a553a58c4333dedb712eee7e2f1716ca0cd
Bastien <bastien.rinck@epitech.eu>
2018-10-14 10:14:15 +0200

    [~] put reading keys from mongo request response

--- src/routes/generate.mjs
+++ src/routes/generate.mjs
@@ -2,16 +2,16 @@ import express from "express";
 import Data from "../mongoose/Data.mjs";
 import Templates from "../mongoose/Templates.mjs";
 import path from "path";
+import Jimp from 'jimp';
 
-const dirname = 'static'
 const router = express.Router();
 
 async function imageFill(template, data, output) {
     return new Promise((resolve, reject) => {
-        Jimp.read(`${config.templates_folder}/${template.path}`, async (err, file) => {
+        Jimp.read(`${template.path}`, async (err, file) => {
             if (err) 
-                throw err;
-            font = await Jimp.loadFont(config.font);
+                reject(err);
+            font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
             for (let field of template.metadata) {
                 let profile_elem = data.find((metadata) => {
                     return metadata.field == field.name;
@@ -36,21 +36,21 @@ router.get('/:id', async (req, res) => {
 
     try {
         const tmplt = await Templates.findById(id).exec();
-        const usr = await Data.find({}).exec();
+        const usr = JSON.parse(JSON.stringify(await Data.find({}).exec()));
         const data = {};
-        
-        //https://stackoverflow.com/questions/17546953/cant-access-object-property-even-though-it-exists-returns-undefined
+
         tmplt.fields.forEach((elem) => {
-            if (!usr[elem.name])
-                return res.status(400).json({message: "Missing fields" })
-            data[elem.name] = usr[elem.name];
+            if (!usr[0][elem.name])
+                return res.status(400).json({message: "Missing fields" })   
+            data[elem.name] = usr[0][elem.name];
         });
 
-        imageFill(tmplt, data, path.join(dirname, tmplt.name + '_filled.jpg')); 
-        res.redirect(path.join(dirname, tmplt.name + '_filled.jpg'));
-    
-    } catch {
-        res.status(404).json({message: "Unknown model"})
+        console.log(path.join("/static", tmplt.name + '_filled.jpg'));
+        imageFill(tmplt, data, path.join("/static", tmplt.name + '_filled.jpg')); 
+        res.redirect(path.join("/static", tmplt.name + '_filled.jpg'));
+    } catch(err) {
+        console.log(err);
+        res.status(404).json({message: "Unknown model", path: path.join(__dirname, "..", tmplt.name + '_filled.jpg')})
     }
     
 });
